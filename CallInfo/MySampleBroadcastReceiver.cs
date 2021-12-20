96896ea7b99bfbef05dd02d6d07066cf4e363a58
Art-afk <a976632@gmail.com>
2021-12-20 21:31:05 +0800

    first commit

---
+++ CallInfo/MySampleBroadcastReceiver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace CallInfo
+{
+
+
+    [BroadcastReceiver(Enabled = true)]
+    [IntentFilter(new string[] { MySampleBroadcastReceiver.MyAction})]
+    public class MySampleBroadcastReceiver : BroadcastReceiver
+    {
+        public const string MyAction = "com.mytest.services.MyReceiver.MyAction";
+        public override void OnReceive(Context context, Intent intent)
+        {
+
+            if (intent.Action == "android.media.RINGER_MODE_CHANGED")
+                Toast.MakeText(context, "Received intent!", ToastLength.Short).Show();
+
+            if (intent != null)
+            {
+                Console.WriteLine("it works");
+            }
+        }
+    }
+}
